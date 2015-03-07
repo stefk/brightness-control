4cda381beb45718ae784a75072ecc4482527de5b
Stéphane Klein <stephaneklein221@gmail.com>
2015-03-07 15:11:45 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var static = require('node-static');
+var socketIo = require('socket.io');
+var fileServer, server, io;
+var brightness = 80;
+
+fileServer = new static.Server('./public');
+
+server = http.createServer(function (request, response) {
+  request.addListener('end', function () {
+    fileServer.serve(request, response);
+  }).resume();
+});
+
+io = socketIo.listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('brightness-changed', brightness);
+  socket.on('brightness-change-request', function (value) {
+    console.log('Setting brightness level to %d%', value);
+    io.sockets.emit('brightness-changed', brightness = value);
+  });
+});
+
+server.listen(8080);
+
+console.log('Server running at http://localhost:8080');
+console.log('Initial brightness level: %d%', brightness);
